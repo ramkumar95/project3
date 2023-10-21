91b3836366e32a2c6dcaf5981384ada0e6d4d57b
ram <ramkumar750786@gmail.com>
2023-10-21 13:46:05 +0530

    test

---
+++ src/main/java/jartest/linux.java
@@ -0,0 +1,52 @@
+package jartest;
+import com.jcraft.jsch.*;
+
+import java.io.InputStream;
+
+public class linux {
+
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        String user = "root";
+        String password = "password";
+        int port = 22;
+
+        try {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec) channel).setCommand("ip addr");
+
+            channel.setInputStream(null);
+            ((ChannelExec) channel).setErrStream(System.err);
+
+            InputStream in = channel.getInputStream();
+
+            channel.connect();
+
+            byte[] tmp = new byte[1024];
+            while (true) {
+                while (in.available() > 0) {
+                    int i = in.read(tmp, 0, 1024);
+                    if (i < 0) break;
+                    System.out.print(new String(tmp, 0, i));
+                }
+                if (channel.isClosed()) {
+                    System.out.println("exit-status: " + channel.getExitStatus());
+                    break;
+                }
+            }
+
+            channel.disconnect();
+            session.disconnect();
+        } catch (JSchException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
